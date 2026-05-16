143bf7ea7f77b52e3bd7593174f25301f0add7d7
VicSerac <victorseraco@hotmail.com>
2026-05-16 04:07:32 -0300

    updated fetch_atnf.py

--- pipeline/fetch_atnf.py
+++ pipeline/fetch_atnf.py
@@ -1,11 +1,16 @@
-from psrqpy import QueryATNF
+import os
+import pathlib
 import pandas as pd
 import numpy as np
-import os, pathlib
-
-os.chdir(pathlib.Path(__file__).parent)
-
-def fetch_atnf_data(output_path = "../data/atnf_raw.parquet"):
+from psrqpy import QueryATNF
+ 
+# Use absolute path so psrqpy cannot affect where we save
+SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
+DATA_DIR = SCRIPT_DIR / "data"
+ 
+def fetch_atnf_data():
+    DATA_DIR.mkdir(exist_ok=True)
+    
     query = QueryATNF(params=[
         'JNAME',
         'RAJ',
@@ -14,20 +19,18 @@ def fetch_atnf_data(output_path = "../data/atnf_raw.parquet"):
         'P0',
         'P1',
     ])
-
+ 
     df = query.pandas
-
+ 
     df.rename(columns={"JNAME": "NS_NAME", "P0": "P", "P1": "PDOT"}, inplace=True)
-    
+ 
     m7_overlap = ["J0720-3125", "J2143+0654", "J1308+2127", "J1856-3754", "J1605+3249", "J0806-4123", "J0420-5022"]
-    
     df = df[~df["NS_NAME"].isin(m7_overlap)]
-
-    # salvar bruto
+ 
+    output_path = str(DATA_DIR / "atnf_raw.parquet")
     df.to_parquet(output_path, index=False)
-
     print(f"Saved raw ATNF data to {output_path}")
-
-
+ 
+ 
 if __name__ == "__main__":
     fetch_atnf_data()
