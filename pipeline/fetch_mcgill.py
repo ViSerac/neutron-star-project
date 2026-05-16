ef23856d8c61bec2f2398982693d37f6dfd662cc
VicSerac <victorseraco@hotmail.com>
2026-05-16 10:05:06 -0300

    fetch_mcgill.py path update

--- pipeline/fetch_mcgill.py
+++ pipeline/fetch_mcgill.py
@@ -1,12 +1,16 @@
 import pandas as pd
-import os, pathlib
+import os
+from pathlib import Path
 
-os.chdir(pathlib.Path(__file__).parent)
+BASE_DIR = Path(__file__).resolve().parent
+DATA_DIR = BASE_DIR / "data"
 
 mcgill_csv = "https://www.physics.mcgill.ca/~pulsar/magnetar/TabO1.csv"
-output_path = "../data/mcgill_raw.parquet"
+output_path = DATA_DIR / "mcgill_raw.parquet"
 
 def fetch_mcgill_data():
+    DATA_DIR.mkdir(parents=True, exist_ok=True)
+    
     df = pd.read_csv(mcgill_csv)
     df.rename(columns={"Dist": "DIST", "Name": "NS_NAME", "RA": "RAJ", "Decl": "DECJ", "RA_Err": "RAJ_ERR", "Decl_Err": "DECJ_ERR", "Period": "P", "Pdot": "PDOT"}, inplace=True)
     df.to_parquet(output_path, index=False)
